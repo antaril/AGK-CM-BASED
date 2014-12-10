63782a997aebd314bec104ef0c97c569950e1aa0
Antaril <Antaril@gmx.de>
2014-12-10 23:13:41 +0100

    add intellim Governor
    add new choosable Over/Underclock option for GPU
    add new choosable Over/Underclock option for CPU
    
    just like in my Lollipop Kernel ;)

--- arch/arm/mach-msm/htc/m7/board-m7-gpu.c
+++ arch/arm/mach-msm/htc/m7/board-m7-gpu.c
@@ -226,30 +226,106 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
+		#ifdef CONFIG_GPU_OVERCLOCK
+                
+#ifdef CONFIG_GPU_UNDERCLOCK
 		{
-			.gpu_freq = 400000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
+                        .gpu_freq = 450000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 400000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 320000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 200000000,
+                        .bus_freq = 3,
+                        .io_fraction = 33,
+                },
+                {
+                        .gpu_freq = 128000000,
+                        .bus_freq = 2,
+                        .io_fraction = 100,
+                },
+                {
+                        .gpu_freq = 64000000,
+                        .bus_freq = 1,
+                        .io_fraction = 100,
+                },
+                {
+                        .gpu_freq = 27000000,
+                        .bus_freq = 0,
+                },
+#else
 		{
-			.gpu_freq = 320000000,
-			.bus_freq = 3,
-			.io_fraction = 33,
-		},
+                        .gpu_freq = 487500000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 450000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 400000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 320000000,
+                        .bus_freq = 3,
+                        .io_fraction = 33,
+                },
+                {
+                        .gpu_freq = 200000000,
+                        .bus_freq = 2,
+                        .io_fraction = 100,
+                },
+                {
+                        .gpu_freq = 128000000,
+                        .bus_freq = 1,
+                        .io_fraction = 100,
+                },
+                {
+                        .gpu_freq = 27000000,
+                        .bus_freq = 0,
+                },
+#endif
+#else
 		{
-			.gpu_freq = 200000000,
-			.bus_freq = 2,
-			.io_fraction = 100,
-		},
+                        .gpu_freq = 400000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
+                {
+                        .gpu_freq = 320000000,
+                        .bus_freq = 3,
+                        .io_fraction = 33,
+                },
+                {
+                        .gpu_freq = 200000000,
+                        .bus_freq = 2,
+                        .io_fraction = 100,
+                },
 		{
-			.gpu_freq = 128000000,
-			.bus_freq = 1,
-			.io_fraction = 100,
-		},
+                        .gpu_freq = 128000000,
+                        .bus_freq = 1,
+                        .io_fraction = 100,
+                },
 		{
-			.gpu_freq = 27000000,
-			.bus_freq = 0,
-		},
+                        .gpu_freq = 27000000,
+                        .bus_freq = 0,
+                },
+                
+#endif
 	},
 	.init_level = 1,
 	.num_levels = 5,
